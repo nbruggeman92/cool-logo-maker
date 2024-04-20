ee55b2e53e98fc2193b45fe03f0e2117cca76151
Nick Bruggeman <nick.bruggeman92@gmail.com>
2024-04-20 12:23:48 -0700

    updated index.js

---
+++ index.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+const inquirer = require("inquirer");
+const colors = require("colors");
+
+const questions = [
+    {
+        type: "input",
+        name: "text",
+        message: "Please enter your text (max 3 characters)",
+    },
+    {
+        type: "input",
+        name: "textColor",
+        message: "Please choose a color for your text (pick either a color keyword or a hexadecimal number)",
+    },
+    {
+        type: "list",
+        name: "shape",
+        message: "Please choose a shape from the following options:",
+        choices: ["circle", "triangle", "square"],
+    },
+    {
+        type: "input",
+        name: "shapeColor",
+        message: "Please choose a color for your shape (pick either a color keyword or a hexadecimal number)",
+    },
+  ].then((data) => {
+    if (data.text.length > 3) {
+        console.log("Your answer must be a maximum of 3 characters");
+    }
+  });
