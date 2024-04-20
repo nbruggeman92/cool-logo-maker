3ab4bdfc08f554e22fa639da4b81f4345486186d
Nick Bruggeman <nick.bruggeman92@gmail.com>
2024-04-20 16:59:57 -0700

    modified shapes.js

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const fs = require("fs");
 const inquirer = require("inquirer");
 const colors = require("colors");
 
-const questions = [
+function promptUser() {
+    inquirer.prompt([
     {
         type: "input",
         name: "text",
@@ -17,15 +18,19 @@ const questions = [
         type: "list",
         name: "shape",
         message: "Please choose a shape from the following options:",
-        choices: ["circle", "triangle", "square"],
+        choices: ["Circle", "Triangle", "Square"],
     },
     {
         type: "input",
         name: "shapeColor",
         message: "Please choose a color for your shape (pick either a color keyword or a hexadecimal number)",
     },
-  ].then((data) => {
-    if (data.text.length > 3) {
-        console.log("Your answer must be a maximum of 3 characters");
+
+    ]).then((data) => {
+        if (data.text.length > 3) {
+            console.log("Your answer must be a maximum of 3 characters");
     }
   });
+}
+
+  promptUser();
